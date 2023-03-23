74f6bba8e2f337c42f6a955420ba64ba412683c9
Cagatay Tetik <99072323@mydavinci.nl>
2023-03-23 11:48:16 +0100

    a

---
+++ module-1/horeca.js
@@ -0,0 +1,4 @@
+let bestelling = prompt("Wat wilt u bestellen?");
+const drankjes = ["fris", "bier", "wijn"];
+const prijzen = {"fris": 1.48, "bier": 2.42, "wijn": 5.99}
+let drankje = ("hoeveel drankjes wilt u bestellen?")
